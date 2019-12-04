6bc32ab66ee874ed082b065f4f9e727b6fdd20ee
Karan Pujara <karanjpujara@gmail.com>
2019-12-04 19:00:24 +0530

    Updates

---
+++ precache-manifest.f3c3a790bbcf37455520d4d68d156116.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7917c1b61d3b66dc26913e76679fe621",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2dc8a73ae2294eb7e944",
+    "url": "/static/css/2.29706000.chunk.css"
+  },
+  {
+    "revision": "068eaac31c99e40a6fb0",
+    "url": "/static/css/main.d294e8d8.chunk.css"
+  },
+  {
+    "revision": "2dc8a73ae2294eb7e944",
+    "url": "/static/js/2.c1891895.chunk.js"
+  },
+  {
+    "revision": "068eaac31c99e40a6fb0",
+    "url": "/static/js/main.0fe65ae7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "68fd86d333768bd4974121be94033595",
+    "url": "/static/media/homepagetop.68fd86d3.png"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/static/media/revicons.2feb69cc.eot"
+  }
+]);
