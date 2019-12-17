038c0f1f42cd21eee08e0af18bf604329d8a8091
Karan Pujara <karanjpujara@gmail.com>
2019-12-17 15:14:26 +0530

    Chnages in eslints

--- src/components/BeforeLogin/ContractorList.js
+++ src/components/BeforeLogin/ContractorList.js
@@ -23,8 +23,9 @@ function ContractorList(props) {
 
     useEffect(() => {
         postContractorDetails();
-    });
-    
+        //eslint-disable-next-line
+    }, []);
+
     async function postContractorDetails() {
         const payload = {
             "city": props.location.state.cityName,
@@ -40,72 +41,72 @@ function ContractorList(props) {
 
     return (<div style={{ marginTop: '-30px' }}>
         <Container className="contractor-list">
-            {contractorData.length ===0 ?<List className="loader"><CircularProgress/></List>:
-            <List className="list">
-                {contractorData.map((cntDetail, index) => {
-                    return (
-                        <div key={index}>
-                            <Link className="underlineNone" to={`/contractordetails/${cntDetail.contractor.id}`}>
-                                <ListItem className="list-item">
-                                    <Grid container spacing={3}>
-                                        <Grid item xs={12} lg={2}>
-                                            {cntDetail.contractor['contractorFiles'].map((item, index) => {
-                                                return (
-                                                    <div key={index}>
-                                                        {item.type === 'AVATAR' ?
-                                                            <img className="myAvatar" alt={item.name} src={`${HttpUrlConstant.BASE_URL}/contractors/${cntDetail.contractor.id}/files/${item.name}`} /> : ' '}
-                                                    </div>
-                                                )
-                                            })}
-                                            {cntDetail.contractor['contractorFiles'].length === 0 ? <img  alt="companyImage" className="displayNone" src={`${HttpUrlConstant.Image_URL}/api/?name=${cntDetail.contractor.address.company}`} /> : ''}
-                                        </Grid>
-                                        <Grid item xs={12} lg={10}>
-                                            <Grid className="row">
-                                                <Grid item xs={12} lg={9}>
-                                                    <ListItemText
-                                                        primary={cntDetail.contractor.address.company} />
-                                                    <div className="d-flex">
-                                                        <span className="rate">{cntDetail.reviewSummary.rating}</span>
-                                                        <div className="stars">
-                                                            <Rating name="size-medium" className="rating r-star"
-                                                                value={cntDetail.reviewSummary.rating}
-                                                                max={5} readOnly
-                                                            />
+            {contractorData.length === 0 ? <List className="loader"><CircularProgress /></List> :
+                <List className="list">
+                    {contractorData.map((cntDetail, index) => {
+                        return (
+                            <div key={index}>
+                                <Link className="underlineNone" to={`/contractordetails/${cntDetail.contractor.id}`}>
+                                    <ListItem className="list-item">
+                                        <Grid container spacing={3}>
+                                            <Grid item xs={12} lg={2}>
+                                                {cntDetail.contractor['contractorFiles'].map((item, index) => {
+                                                    return (
+                                                        <div key={index}>
+                                                            {item.type === 'AVATAR' ?
+                                                                <img className="myAvatar" alt={item.name} src={`${HttpUrlConstant.BASE_URL}/contractors/${cntDetail.contractor.id}/files/${item.name}`} /> : ' '}
                                                         </div>
-                                                        <span className="views">{cntDetail.reviewSummary.totalReviews}&nbsp;reviews</span>
-                                                    </div>
-                                                </Grid>
-                                                <Grid item xs={12} lg={3}>
-                                                    <div className="no-margin"><i className="fa fa-comment-o" aria-hidden="true"></i></div>
-                                                    <span style={{ fontSize: '13px', color: 'rgba(0, 0, 0, 0.54)' }}>contact for price</span>
-                                                </Grid>
+                                                    )
+                                                })}
+                                                {cntDetail.contractor['contractorFiles'].length === 0 ? <img alt="companyImage" className="displayNone" src={`${HttpUrlConstant.Image_URL}/api/?name=${cntDetail.contractor.address.company}`} /> : ''}
                                             </Grid>
-                                            <Grid item xs={12} lg={12} className="mtmb">
-                                                <span className="watch-icon"> <AccessTimeIcon style={{ color: '#817c7c' }} /> </span>
-                                                <span className="years">
-                                                    {date - cntDetail.contractor.address.founded}
-                                                    &nbsp;years in business</span>
-                                            </Grid>
-                                            <Grid container className="row fullwidth">
-                                                <Grid item xs={12} lg={9}>
-                                                    <div className="details">
-                                                        "{cntDetail.reviewSummary.areview}"
-                                                    </div>
+                                            <Grid item xs={12} lg={10}>
+                                                <Grid className="row">
+                                                    <Grid item xs={12} lg={9}>
+                                                        <ListItemText
+                                                            primary={cntDetail.contractor.address.company} />
+                                                        <div className="d-flex">
+                                                            <span className="rate">{cntDetail.reviewSummary.rating}</span>
+                                                            <div className="stars">
+                                                                <Rating name="size-medium" className="rating r-star"
+                                                                    value={cntDetail.reviewSummary.rating}
+                                                                    max={5} readOnly
+                                                                />
+                                                            </div>
+                                                            <span className="views">{cntDetail.reviewSummary.totalReviews}&nbsp;reviews</span>
+                                                        </div>
+                                                    </Grid>
+                                                    <Grid item xs={12} lg={3}>
+                                                        <div className="no-margin"><i className="fa fa-comment-o" aria-hidden="true"></i></div>
+                                                        <span style={{ fontSize: '13px', color: 'rgba(0, 0, 0, 0.54)' }}>contact for price</span>
+                                                    </Grid>
                                                 </Grid>
-                                                <Grid item xs={12} lg={3} >
-                                                    <Button variant="contained" color="primary" >
-                                                        View Profile
+                                                <Grid item xs={12} lg={12} className="mtmb">
+                                                    <span className="watch-icon"> <AccessTimeIcon style={{ color: '#817c7c' }} /> </span>
+                                                    <span className="years">
+                                                        {date - cntDetail.contractor.address.founded}
+                                                        &nbsp;years in business</span>
+                                                </Grid>
+                                                <Grid container className="row fullwidth">
+                                                    <Grid item xs={12} lg={9}>
+                                                        <div className="details">
+                                                            "{cntDetail.reviewSummary.areview}"
+                                                    </div>
+                                                    </Grid>
+                                                    <Grid item xs={12} lg={3} >
+                                                        <Button variant="contained" color="primary" >
+                                                            View Profile
                                                     </Button>
+                                                    </Grid>
                                                 </Grid>
                                             </Grid>
                                         </Grid>
-                                    </Grid>
-                                </ListItem>
-                            </Link>
-                            <Divider />
-                        </div>)
-                })}
-            </List>
+                                    </ListItem>
+                                </Link>
+                                <Divider />
+                            </div>)
+                    })}
+                </List>
             }
         </Container>
     </div>
