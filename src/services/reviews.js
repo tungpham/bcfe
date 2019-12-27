02c6a9296c17451853305c15e76ebd15e0847b3a
ikramcoder1988 <ikramcoder1988@gmail.com>
2019-12-28 01:10:06 +0800

    change url for writing review

--- src/services/reviews.js
+++ src/services/reviews.js
@@ -1,7 +1,7 @@
 import Axios from 'axios';
 
-const REVIEWS_API_PATH = process.env.REACT_APP_PROJECT_API + 'reviews/';
-
+const REVIEWS_API_PATH = process.env.REACT_APP_PROJECT_API + 'contractors/reviews/';
+                         
 export default {
 	submit: (id, params) => Axios.post(REVIEWS_API_PATH + id + "/write" , params),
 };
